ab5e815425068fd17f7cf684b0006423ee9637a2
Пользователь <Valenta2910@mail.ru>
2023-05-22 21:02:15 +0300

    initial commit

--- src/main/java/AppConfig.java
+++ src/main/java/AppConfig.java
@@ -1,2 +1,35 @@
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class AppConfig {
+    @Bean
+    public Transport car() {
+        return new Car("Car");
+    }
+
+    @Bean
+    public Transport bus() {
+        return new Bus("Bus");
+    }
+
+    @Bean
+    public Transport pickup() {
+        return new Pickup("Pickup");
+    }
+
+    @Bean(name = "driver1")
+    public Driver driver1() {
+        return new Driver("Vadim", car());
+    }
+
+    @Bean(name = "driver2")
+    public Driver driver2() {
+        return new Driver("Valentin", bus());
+    }
+
+    @Bean(name = "driver3")
+    public Driver driver3() {
+        return new Driver("Boris", pickup());
+    }
 }
